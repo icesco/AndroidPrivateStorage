85fca5819ee760901f19c11b6b45cc02a3fbfc7a
Francesco Bianco <francesco.bianco@me.com>
2025-05-15 11:29:56 +0200

    Adds publishing rule

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.serialization)
+    id("maven-publish")
 }
+
+val libraryVersion = "1.0.0"
 
 android {
     namespace = "tech.scytale.security.androidprivatestorage"
@@ -43,4 +46,37 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    androidTestImplementation(project(path =":AndroidPrivateStorage"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("aar") {
+            groupId = "tech.scytale"
+            artifactId = project.name
+            version = libraryVersion
+
+            artifact(
+                if (libraryVersion.endsWith("DEBUG")) {
+                    File("$buildDir/outputs/aar/${project.name}-codedebug.aar")
+                } else {
+                    File("$buildDir/outputs/aar/${project.name}-release.aar")
+                }
+            )
+
+            pom.withXml {
+                val dependenciesNode = asNode().appendNode("dependencies")
+                configurations.getByName("releaseCompileClasspath")
+                    .resolvedConfiguration
+                    .firstLevelModuleDependencies
+                    .forEach {
+                        val depNode = dependenciesNode.appendNode("dependency")
+                        depNode.appendNode("groupId", it.moduleGroup)
+                        depNode.appendNode("artifactId", it.moduleName)
+                        depNode.appendNode("version", it.moduleVersion)
+                    }
+            }
+        }
+    }
 }
